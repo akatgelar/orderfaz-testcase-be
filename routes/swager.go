bfb27b2c63b3f19064a2aa620e9b07fc28134a47
akatgelar <akatgelar@gmail.com>
2025-07-31 16:08:44 +0700

    feat: swagger doc

---
+++ routes/swager.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"akatgelar/orderfaz-testcase-be/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+func Swager(g *gin.RouterGroup) { 
+	docs.SwaggerInfo.Title = "Orderfaz Testcase API"
+	docs.SwaggerInfo.Description = "This is a sample server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"} 
+	g.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
